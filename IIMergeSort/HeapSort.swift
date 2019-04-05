ca2cb85c606b60c881fbbb21a17ae2ce3a967e3e
Noah_Shan <shanwzh@inspur.com>
2019-04-05 10:41:57 +0800

    ====Noah Shan====
    init
    
    ====END====

---
+++ IIMergeSort/HeapSort.swift
@@ -0,0 +1,93 @@
+//
+//  *******************************************
+//  
+//  HeapSort.swift
+//  impcloud_dev
+//
+//  Created by Noah_Shan on 2019/2/21.
+//  Copyright © 2018 Inpur. All rights reserved.
+//  
+//  *******************************************
+//
+
+import UIKit
+
+/*
+ HeapSort-堆排序
+ 思路：
+ 1.如果最终结果需要是升序，则需要使用大顶堆（根最大），反之使用小顶堆（根最小）]
+
+ 2.实现思路：下面以升序为例
+ a.将无需数组构造为一个堆结构[还是在数组中表示]
+ b.然后将根节点和最末尾的节点交换位置
+ c.交过位置过后，再次生成一个堆结构（排除最末尾最大的那个节点）
+ d.再次将根（最大值）和堆中最末尾交换  重复cd两步
+
+ HeapSort是不稳定排序<排序前俩相同的元素AB,排序后，他俩顺序不变则表示稳定>
+ */
+
+enum HeapSortType: Int {
+    case asc
+    case desc
+}
+
+/// 私有拓展
+private extension Int {
+
+    var parent: Int {
+        return (self - 1) / 2
+    }
+
+    var leftChild: Int {
+        return (self * 2) + 1
+    }
+
+    var rightChild: Int {
+        return (self * 2) + 2
+    }
+}
+
+public extension Array where Element: Comparable {
+    public mutating func heapSort() {
+        buildHeap()
+        shrinkHeap()
+    }
+
+    private mutating func buildHeap() {
+        for index in 1..<self.count {
+            var child = index
+            var parent = child.parent
+            while child > 0 && self[child] > self[parent] {
+                swapAt(child, parent)
+                child = parent
+                parent = child.parent
+            }
+        }
+    }
+
+    private mutating func shrinkHeap() {
+        for index in stride(from: self.count - 1, to: 0, by: -1) {
+            swapAt(0, index)
+            var parent = 0
+            var leftChild = parent.leftChild
+            var rightChild = parent.rightChild
+            while parent < index {
+                var maxChild = -1
+                if leftChild < index {
+                    maxChild = leftChild
+                } else {
+                    break
+                }
+                if rightChild < index && self[rightChild] > self[maxChild] {
+                    maxChild = rightChild
+                }
+                guard self[maxChild] > self[parent] else { break }
+
+                swapAt(parent, maxChild)
+                parent = maxChild
+                leftChild = parent.leftChild
+                rightChild = parent.rightChild
+            }
+        }
+    }
+}
---
+++ IISwiftBaseUti.podspec
@@ -0,0 +1,45 @@
+#
+# Be sure to run `pod lib lint IISwiftBaseUti.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+  s.name             = 'IISwiftBaseUti'
+  s.version          = '0.1.0'
+  s.summary          = 'IISwift基础服务'
+
+# This description is used to generate tags and improve search results.
+#   * Think: What does it do? Why did you write it? What is the focus?
+#   * Try to keep it short, snappy and to the point.
+#   * Write the description between the DESC delimiters below.
+#   * Finally, don't worry about the indent, CocoaPods strips it!
+
+  s.description      = <<-DESC
+     包含：
+     1.多种破次方排序方法（heapsort/mergesort)
+     2.自定义轻量级队列（IIQueue)
+     3.GCD二次封装（GCGUtils）
+                       DESC
+
+  s.homepage         = 'https://github.com/hatjs880328s/IISwiftBaseUti'
+  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'hatjs880328s' => 'shanwzh@inspur.com' }
+  s.source           = { :git => 'https://github.com/hatjs880328s/IISwiftBaseUti.git', :tag => s.version.to_s }
+  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+
+  s.ios.deployment_target = '8.0'
+
+  s.source_files = 'IISwiftBaseUti/Classes/**/*'
+  
+  # s.resource_bundles = {
+  #   'IISwiftBaseUti' => ['IISwiftBaseUti/Assets/*.png']
+  # }
+
+  # s.public_header_files = 'Pod/Classes/**/*.h'
+  # s.frameworks = 'UIKit', 'MapKit'
+  # s.dependency 'AFNetworking', '~> 2.3'
+end
